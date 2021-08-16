a5a0d0e2e2b2c5aaa0410056aa233e262d084823
RohanNavlakhe <rohannavlakhe2@gmail.com>
2021-08-16 16:28:55 +0530

    double voice issue fixed

--- public/room.js
+++ public/room.js
@@ -5,18 +5,30 @@ var peer = new Peer(/*undefined,{
 
 const $videoContainer = document.querySelector('#video-grid')
 let myStreamToPassToRemote
+let localStream
 
 
 navigator.mediaDevices
     .getUserMedia({video: true, audio: true})
     .then((stream) => {
         myStreamToPassToRemote = stream
-       /* const localStream = stream.clone()
+        /*const */localStream = stream.clone()
+
+        //making local stream audio and video disabled
+        //local stream is used to show user his/her own video preview
+        //so for local stream audio will always be off otherwise we will hear our own voice
+        //and video state of local stream will be same as remote stream (which we're
+        //passing to other users).Means if user clicks of start video  btn
+        // then user will be able to see his/her preview
+
         localStream.getAudioTracks()[0].enabled = false;
-        initVideoStream(localStream,prepareVideoElement())*/
+        localStream.getVideoTracks()[0].enabled = false;
+        initVideoStream(localStream,prepareVideoElement())
+
+        //mute the audio and stop the video (means other users neither hear your voice not see you)
         mute()
         stopVideo()
-        initVideoStream(stream,prepareVideoElement())
+        //initVideoStream(localStream,prepareVideoElement())
 
         //Provide your own stream if you're a new joiner (when someone calls for your stream)
         peer.on('call', function(call) {
@@ -86,14 +98,16 @@ const mute = () => {
 }
 
 const playStop = () => {
-    console.log('object')
+    //console.log('object')
     let enabled = myStreamToPassToRemote.getVideoTracks()[0].enabled;
     if (enabled) {
         myStreamToPassToRemote.getVideoTracks()[0].enabled = false;
+        localStream.getVideoTracks()[0].enabled = false;
         setPlayVideo()
     } else {
         setStopVideo()
         myStreamToPassToRemote.getVideoTracks()[0].enabled = true;
+        localStream.getVideoTracks()[0].enabled = true;
     }
 }
 
